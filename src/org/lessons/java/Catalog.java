3685d5fea70cc399944448db17932bd286057c0a
marcantonio99 <marcantonio8799@gmail.com>
2023-06-07 15:30:04 +0200

    start Catalog

---
+++ src/org/lessons/java/Catalog.java
@@ -0,0 +1,9 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Catalog {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
